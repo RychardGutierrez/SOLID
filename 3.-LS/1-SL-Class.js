b388532bc166674fe0a07ac4f76dd5b7ef3a95e7
Richard Gutierrez Condori <rychard.gutierrez@gmail.com>
2024-02-22 23:48:00 -0400

    solid principles examples

---
+++ 3.-LS/1-SL-Class.js
@@ -0,0 +1,33 @@
+/**
+ * if a class is a subclass of another class, it should be able to be used wherever 
+ * its parent class is used, without causing issues
+ */
+
+class PaymentGateway {
+  processPayment(amount) {
+    // Logic to process payment
+    console.log(`Processing payment of $${amount}`);
+  }
+}
+
+class SecurePaymentGateway extends PaymentGateway {
+  processPayment(amount) {
+    // Additional security steps
+    this.authenticateUser();
+    super.processPayment(amount);
+  }
+
+  authenticateUser() {
+    console.log('User authenticated securely.');
+  }
+}
+
+function makePayment(paymentGateway, amount) {
+  paymentGateway.processPayment(amount);
+}
+
+const basicGateway = new PaymentGateway();
+const secureGateway = new SecurePaymentGateway();
+
+makePayment(basicGateway, 100); // Output: Processing payment of $100
+makePayment(secureGateway, 100); // Output: User authenticated securely. Processing payment of $100
